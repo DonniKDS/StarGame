a4d5b57a273072f838e06ce6a391a5f8f5ddc171
Donni <donnikds@gmail.com>
2020-04-25 19:46:06 +0300

    Homework1

--- desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import ru.geekbrains.StarGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.x = 200;
+		config.y = 50;
+		config.height = 640;
+		config.width = 360;
 		new LwjglApplication(new StarGame(), config);
 	}
 }
