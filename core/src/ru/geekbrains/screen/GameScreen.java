a6e08714cb228c13731cb0692c3ec50052adb2f8
Donni <donnikds@gmail.com>
2020-05-07 15:22:11 +0300

    homework3 main task

--- core/src/ru/geekbrains/screen/GameScreen.java
+++ core/src/ru/geekbrains/screen/GameScreen.java
@@ -6,22 +6,28 @@ import com.badlogic.gdx.math.Vector2;
 import ru.geekbrains.base.BaseScreen;
 import ru.geekbrains.math.Rect;
 import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.Starship;
 
-public class MenuScreen extends BaseScreen {
+public class GameScreen extends BaseScreen {
 
     private Texture bg;
+    private Texture ss;
     private Background background;
+    private Starship starship;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("background.jpg");
+        ss = new Texture("starship.png");
         background = new Background(bg);
+        starship = new Starship(ss);
     }
 
     @Override
     public void resize(Rect worldBounds) {
         background.resize(worldBounds);
+        starship.resize(worldBounds);
     }
 
     @Override
@@ -29,17 +35,20 @@ public class MenuScreen extends BaseScreen {
         super.render(delta);
         batch.begin();
         background.draw(batch);
+        starship.draw(batch);
         batch.end();
     }
 
     @Override
     public void dispose() {
         bg.dispose();
+        ss.dispose();
         super.dispose();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
+        starship.touchDown(touch, pointer, button);
         return super.touchDown(touch, pointer, button);
     }
 
